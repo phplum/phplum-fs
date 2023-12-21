e9d8234609e2754bf8b77babcdb650d77159009c
Garrett HE <garrett.he@outlook.com>
2023-12-21 11:47:42 +0800

    Add property Path::parent

--- src/Path.php
+++ src/Path.php
@@ -3,10 +3,13 @@
 namespace phplum\fs;
 
 use Stringable;
+use RuntimeException;
 
 /**
  * Performs operations on file or directory path information with a
  * cross-platform manner.
+ *
+ * @property Path $parent
  */
 class Path implements Stringable
 {
@@ -26,6 +29,23 @@ class Path implements Stringable
     {
         $this->segments = array_filter($segments);
     }
+
+    /**
+     * __get
+     *
+     * @param string $name Property name.
+     *
+     * @return Path Property value.
+     * @throws RuntimeException If getting unknown properties.
+     */
+    public function __get(string $name)
+    {
+        return match ($name) {
+            'parent' => new Path(...array_slice($this->segments, 0, (count($this->segments) - 1))),
+            default => throw new RuntimeException('Getting unknown property: ' . get_class($this) . '::' . $name),
+        };
+    }
+
 
     /**
      * Returns a string representation according to current operating system.
